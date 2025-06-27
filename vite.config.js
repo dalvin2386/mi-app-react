05dee646493a425d607867cdb0254e7184875b73
dalvin2386 <dalvinsoriano@gmail.com>
2025-06-26 22:07:22 -0600

    cambio en vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // ¡Añade esta línea!
+  // 'base' es la URL base para el despliegue de tu aplicación.
+  // Debe coincidir con el nombre de tu repositorio en GitHub Pages,
+  // rodeado de barras diagonales.
+  base: '/mi-app-react/',
   plugins: [react()],
 })
